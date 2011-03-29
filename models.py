8c78693981160bcd0443177465599cfe3a73475e
Brian Tubergen <brian.tubergen@gmail.com>
2011-03-29 17:49:59 -0400

    added model classes. have not been tested.

--- models.py
+++ models.py
@@ -1,32 +1,67 @@
 from django.db import models
 
 class Post(models.Model):
-    name = models.CharField(max_length=30)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+    group = models.ForeignKey(Group)
+    author = models.ForeignKey(User)
+    text = models.TextField()
+    date = models.DateField(auto_now=True)
+    upvotes = models.IntegerField() 
+    downvotes = models.IntegerField() 
+    spamvotes = models.IntegerField() 
+
+    '''"Comment List" represented implicitly. Given a post, we should be able
+    to get the the associated comments through the foreign key relationship.
+    '''
 
     class Meta:
-        abstract = True
+        abstract = True  # post instances cannot be declared
 
     def __unicode__(self):
-        return self.name
+        return "Post by" + self.author + "on" + self.date;
 
-class Author(models.Model):
+class Announcement(models.Post):
+    pass
+
+class Event(models.Post):
+    eventDate = models.DateField()    
+    flags = models.ManyToManyField(Flag)
+
+# We should only associate a comment with either an event or an announcement
+# Not both
+class Comment(models.Post):
+    announcement = models.ForeignKey(Announcement)
+    event = models.ForeignKey(Event)
+
+class Flag(models.Model):
+    name = models.charField(max_length=30)
+    
+    def __unicode__(self):
+        return self.name;
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey(Group, related_name="child_set")
+    posts = models.ManyToManyField(Post)
+    members = models.ManyToManyField(User)
+    admins = models.ManyToManyField(User)
+    subscribers = models.ManyToManyField(User)
+    email_subscribers = models.ManyToManyField(User)
+    tags = models.ManyToManyField(Tag)
+    groupinfo = models.OneToOneField(GroupInfo)
+
+    '''Announcements, Events represented through foreignKey relationship.'''
+
+class GroupInfo(models.Model):
+    pass
+
+class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+    userinfo = models.OneToOneField(UserInfo)
 
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
-
-    def __unicode__(self):
-        return self.title
+class UserInfo(models.Model):
+    pass
--- settings.py
+++ settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'mysql', 
+        'NAME': 'django_database',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',      
+        'PORT': '',      
     }
 }
 
@@ -90,7 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
