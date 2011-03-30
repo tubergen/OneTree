51c3ee7f108e121c9a7130a2464bfd0865beacfc
Brian Tubergen <brian.tubergen@gmail.com>
2011-03-30 19:08:45 -0400

    small bug fix

--- wall/views.py
+++ wall/views.py
@@ -14,12 +14,13 @@ this would not be necessary if posts were not abstract?
 
 def group(request):
     errormsg = None
-    if 'post_content' in request.POST and request.POST['post_content']:
-        # later insert logic to distinguish events vs announcements
-        new_announcement = Announcement(text=request.POST['post_content'])
-        new_announcement.save()
-    else:
-        errormsg = "Empty announcement? Surely you aren't *that* boring."
+    if request.method == 'POST':
+       if 'post_content' in request.POST and request.POST['post_content']:
+           # later insert logic to distinguish events vs announcements
+           new_announcement = Announcement(text=request.POST['post_content'])
+           new_announcement.save()
+       else:
+           errormsg = "Empty announcement? Surely you aren't *that* boring."
     announcements = Announcement.objects.all()
     return render_to_response('group_page.html',
                               {'announcements': announcements,
