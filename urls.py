4b22df26e0e1eb4ab5747d1410c927ce296bc730
Ming <mchng@princeton.edu>
2011-04-01 13:16:16 -0400

    touched

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from OneTree.apps.group_page.views import group_page
+from OneTree.apps.group_signup.views import create_group
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -17,6 +18,7 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     #(r'^group/$', group_page),
     (r'^group/(\w+)/$', group_page),                       
+    (r'^group-signup/$', create_group),
 )
 
 # on our own computers, serve static files properly
