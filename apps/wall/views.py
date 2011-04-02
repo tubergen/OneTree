2c9955ceeb6d0c905dc95555a0379b7b9eb420c5
Brian Tubergen <brian.tubergen@gmail.com>
2011-04-02 04:02:32 -0400

    fixed a small arithmetic error

--- apps/wall/views.py
+++ apps/wall/views.py
@@ -12,7 +12,6 @@ def update_vote(request):
         ann_id = request.GET.get("ann_id")
         vote_type = request.GET.get("vote_type")
         if ann_id and vote_type:
-            print 'not none'
             ann = Announcement.objects.get(id=ann_id)
             if ann:
                 if vote_type == 'up':
@@ -22,7 +21,7 @@ def update_vote(request):
                         ann.upvotes += 1
                 else:
                     if ann.downvotes == None:
-                        ann.downvotes = -1
+                        ann.downvotes = 1
                     else:
                         ann.downvotes += 1
                 ann.save()
