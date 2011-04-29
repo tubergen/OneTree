0bbbe75866997e79be282ef81d5c3f65a1980099
Tyler Brown <tabrown@princeton.edu>
2011-04-28 20:22:42 -0400

    Fixed group info error

--- apps/group/views.py
+++ apps/group/views.py
@@ -436,7 +436,7 @@ def groupinfo_page(request, groupname):
 
     # handle editable info submit
     if 'data_submit' in request.POST:
-        errormsg = handle_data(groupinfo, request)
+        errormsg = handle_data(groupinfo, this_group, request)
 
 # ORIGINAL END OF GROUPINFO
 
@@ -567,15 +567,16 @@ def groupphotos_page(request, groupname):
     pass
 
 
-def handle_data(groupinfo, request):
+def handle_data(groupinfo, group, request):
     errormsg = None
-    group = groupinfo.group
+    this_group = group
     if request.method == 'POST':
-        if not verify_admin(request, group):
+        if not verify_admin(request, this_group):
             errormsg = "You are not an admin!"
         else:
             new_data = request.POST['data_content']
             groupinfo.data = new_data
+            groupinfo.group = this_group
             groupinfo.save()
 
     return errormsg
