9d7d9430e43949ba942d1d983b9106ae1c0d47fb
Ming Loong Chng <mchng@princeton.edu>
2011-05-05 04:27:43 -0400

    fixed top navigation problem

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -4,11 +4,14 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
 
 def login(request, template_name='registration/login.html',
           redirect_field_name=REDIRECT_FIELD_NAME,
           redirect_authenticated=None,
           authentication_form=AuthenticationForm):
+    print "========= In apps/auth/login ============"
     context=RequestContext(request)
 
     form = authentication_form(data=request.POST)
@@ -16,23 +19,25 @@ def login(request, template_name='registration/login.html',
     if request.method == "POST":
         print "username: ",
         print form.data['username']
-
         try:
             user = User.objects.get(username=form.data['username'])
-        except:
+
+        except:            
+            user = User.objects.get(username='') # needed to prevent using user.is_authenticated() NEED A BETTER WAY TO AVOID THIS!
             return django_login(request, template_name=template_name,
                                 redirect_field_name=redirect_field_name,
                                 authentication_form=authentication_form)
-
+        
         if user.is_active == False:
             print "NOT ACTIVATED YET"
+            user_name = user.username
 
             return render_to_response('base_login.html', 
                                       { 'form': form, 
-                                        'user': user,
+                                        'username': user_name,
                                         'need_activation': True,
                                         },
-                                      context_instance=context)
+                                      context_instance=RequestContext(request))
             
 
 
